8719233fab328318a9ed427668518be0cd72db3b
apt47 <57804710+MALW4R3@users.noreply.github.com>
2021-10-09 18:37:43 +0700

    Update SHOCK.c

--- SHOCK.c
+++ SHOCK.c
@@ -4,8 +4,8 @@
 int main(int argc, char *argv[]) {
         if(argc==2) {
 		printf("Checking License: %s\n", argv[1]);
-		if(strcmp(argv[1], "AAAA-Z10N-4123-31231312")==0) {
-			printf("flag{39fc39c90c5a1a16c115b0dcd073b8362c4667fb}\n");
+		if(strcmp(argv[1], "HEHEBOI")==0) {
+			printf("flag{......}\n");
 		} else {
 			printf("WRONG!\n");
 		}
